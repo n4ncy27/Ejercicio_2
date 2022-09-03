7b2b94db7b6eebce99bdef2c62c6635e20764cb9
Nancy Liliana Sáenz Moreno <111588657+n4ncy27@users.noreply.github.com>
2022-09-02 22:12:04 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ejercicio No.3: Inversor de numeros enteros de cuatro digitos.
+
+## Diagrama de flujo
+
+![Diagrama flujo](diagrama.png "Diagrama de flujo")
---
+++ inversor_enteros_4_digitos.py
@@ -0,0 +1,16 @@
+#Programa No 3: Inversor de numeros enteros de cuatro digitos.
+
+print("--------------------")
+print("--INVERSOR-NUMEROS--")
+print("--------------------")
+print()
+
+#input
+n1 = input("Digíte el primer número: ")
+n2 = input("Digíte el segundo número: ")
+n3 = input("Digíte el tercer número: ")
+n4 = input("Digíte el cuarto número: ")
+
+#i = n
+
+print( "El número inverso es:" + str(n4) + str(n3) + str(n2) + str(n1))
